6bc60b3171f385dd3102d7e3e30ae8cff18aaeac
Ray Gregory <greatgrandpadog@gmail.com>
2011-11-11 16:21:26 -0600

    Added p43

--- solutions.py
+++ solutions.py
@@ -462,13 +462,55 @@ def p42():
         if sum(let_dict[let] for let in word) in tnumbers:
             triangle_word_count[0] = triangle_word_count[0] + 1
 
-    word_reader = csv.reader(open('words.txt', 'rb'), delimiter=',', quotechar='"')
+    word_reader = csv.reader(open('words.txt', 'rb'), delimiter = ',', quotechar = '"')
     for word_list in word_reader:
         for word in word_list:
             __check_word_for_triangle(word)      
          
     return triangle_word_count[0]
 
+def p43():
+    """The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property.
+    Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+    d2d3d4=406 is divisible by 2
+    d3d4d5=063 is divisible by 3
+    d4d5d6=635 is divisible by 5
+    d5d6d7=357 is divisible by 7
+    d6d7d8=572 is divisible by 11
+    d7d8d9=728 is divisible by 13
+    d8d9d10=289 is divisible by 17
+    Find the sum of all 0 to 9 pandigital numbers with this property."""
+
+    pandig_sum = [0]
+    
+    def __build_pandigs(chosen_digs, digits_avail):
+        for d1 in digits_avail:
+            snum = chosen_digs + d1
+            if snum == '0':
+                continue
+            if len(snum) == 4 and int(snum[1:4]) % 2:
+                continue
+            if len(snum) == 5 and int(snum[2:5]) % 3:
+                continue
+            if len(snum) == 6 and int(snum[3:6]) % 5:
+                continue
+            if len(snum) == 7 and int(snum[4:7]) % 7:
+                continue
+            if len(snum) == 8 and int(snum[5:8]) % 11:
+                continue
+            if len(snum) == 9 and int(snum[6:9]) % 13:
+                continue
+            if len(snum) >= 10:
+                if int(snum[7:10]) %17:
+                    continue
+                pandig_sum[0] += int(snum)
+                continue
+            else:
+                __build_pandigs(snum, digits_avail - set(d1))
+    
+    __build_pandigs('',set('0123456789'))
+    
+    return pandig_sum[0]
 
 def p48():
     """Find the last ten digits of the number 1^1 ....1000^1000"""
